4298fdebb3e8deb20960a97381adb638c18663b5
RajeevPatel <rajeev.patel@areteminds.com>
2025-03-15 13:34:29 +0530

    code merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import UsingUnstop from "./components/who's/usingUnstop";
-import Unlock from "./components/unlock/unlock";
 
 function App() {
   return (
     <div>
-      <Unlock />
       <UsingUnstop />
     </div>
   );
